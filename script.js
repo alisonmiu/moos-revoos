444ec4a2d9c243771df085a6fd74a608fa7ed391
Alison Miu <alisonmiu@gmail.com>
2022-09-19 21:56:59 +1000

    updated class names

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 'use strict'
- function createPopupWin(pageURL, pageTitle,
-                    popupWinWidth, popupWinHeight) {
-            var left = (screen.width ) ;
-            var top = (screen.height ) ;
-            var myWindow = window.open(pageURL, pageTitle,
-                    'resizable=yes, width=' + popupWinWidth
-                    + ', height=' + popupWinHeight + ', top='
-                    + top + ', left=' + left);
-        }
+
+const items = document.querySelectorAll('.modals .movieDesc');
+
+items.forEach((item) => {
+	item.querySelector('img').addEventListener('click', (img) => {
+		item.querySelector('dialog').showModal();
+	});
+
+	item.querySelector('button.close').addEventListener('click', (btn) => {
+		btn.target.parentNode.close();
+	})
+});
